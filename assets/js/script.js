6d502386d0945b5ec64aca17eb6a964a8ebe71ab
DOJ85 <florianpelloux85@gmail.com>
2018-04-03 09:47:20 +0200

    fix all responsiv issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
 
 function randombg(){
-  var random= Math.floor(Math.random() * 18) + 0;
-  var bigSize = ["url('assets/img/img1.jpg')",
-                 "url('assets/img/img2.jpg')",
-                 "url('assets/img/img3.jpg')",
-                 "url('assets/img/img4.jpg')",
-                 "url('assets/img/img5.jpg')",
-                 "url('assets/img/img6.jpg')",
-                 "url('assets/img/img7.jpg')",
-                 "url('assets/img/img8.jpg')",
-                 "url('assets/img/img9.jpg')",
-                 "url('assets/img/img10.jpg')",
-                 "url('assets/img/img11.jpg')",
-                 "url('assets/img/img12.jpg')",
-                 "url('assets/img/img13.jpg')",
-                 "url('assets/img/img14.jpg')",
-                 "url('assets/img/img15.jpg')",
-                 "url('assets/img/img16.jpg')",
-                 "url('assets/img/img17.jpg')",
-                 "url('assets/img/img18.jpg')"];
+  var random= Math.floor(Math.random() * 23) + 0;
+  var bigSize = ["url('assets/img/bg/img1.jpg')",
+                 "url('assets/img/bg/img2.jpg')",
+                 "url('assets/img/bg/img3.jpg')",
+                 "url('assets/img/bg/img4.jpg')",
+                 "url('assets/img/bg/img5.jpg')",
+                 "url('assets/img/bg/img6.jpg')",
+                 "url('assets/img/bg/img7.jpg')",
+                 "url('assets/img/bg/img8.jpg')",
+                 "url('assets/img/bg/img9.jpg')",
+                 "url('assets/img/bg/img10.jpg')",
+                 "url('assets/img/bg/img11.jpg')",
+                 "url('assets/img/bg/img12.jpg')",
+                 "url('assets/img/bg/img13.jpg')",
+                 "url('assets/img/bg/img14.jpg')",
+                 "url('assets/img/bg/img15.jpg')",
+                 "url('assets/img/bg/img16.jpg')",
+                 "url('assets/img/bg/img17.jpg')",
+                 "url('assets/img/bg/img18.jpg')",
+                 "url('assets/img/bg/img19.png')",
+                 "url('assets/img/bg/img20.png')",
+                 "url('assets/img/bg/img21.png')",
+                 "url('assets/img/bg/img22.png')",
+                 "url('assets/img/bg/img23.jpg')"];
   document.getElementById("random").style.backgroundImage=bigSize[random];
 }
